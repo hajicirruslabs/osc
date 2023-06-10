ce1840593d20ecf5fc69db23a164cb858a11bd7f
ericggul <57379903+ericggul@users.noreply.github.com>
2023-06-11 00:01:39 +0100

    Feat: Waiting UI

--- containers/screen/Waiting/index.js
+++ containers/screen/Waiting/index.js
@@ -1,6 +1,9 @@
 import * as S from "./styles";
 
 import { useState, useEffect, Suspense } from "react";
+import { TbTriangleFilled, TbTriangleInvertedFilled } from "react-icons/tb";
+
+const DUMMY_DATA = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
 
 export default function Comp() {
   return (
@@ -8,6 +11,157 @@ export default function Comp() {
       <S.LogoContainer>
         <img src="/assets/images/logo-small.svg" alt="logo" />
       </S.LogoContainer>
+
+      <S.Left>
+        <Currency />
+        <S.Commercial></S.Commercial>
+        <S.QRContainer>
+          <S.QRWrapper>
+            <img src="/assets/screen/QR.svg" alt="qr" />
+          </S.QRWrapper>
+
+          <S.Text>
+            <p>Your region is the 8th highest performing in the current climate zone.</p>
+            <p>Please scan to contribute to its longevity.</p>
+          </S.Text>
+        </S.QRContainer>
+      </S.Left>
+      <S.Right>
+        <h1>Top 9 Flourishing Regions of the Temperates</h1>
+        <S.RankingSection>
+          {DUMMY_DATA.map((datum, i) => (
+            <S.SingleEl key={i} border={Math.random() < 0.3}>
+              {i < 3 && (
+                <S.Ranking big={i === 0}>
+                  <img src={`/assets/screen/Ribbon-${i + 1}.svg`} alt="ranking" />
+                </S.Ranking>
+              )}
+            </S.SingleEl>
+          ))}
+        </S.RankingSection>
+        <S.BottomInfo>
+          <S.NewsSection>
+            <S.SingleNewsEl>
+              <S.NewsLeft>
+                <h3>Urban Times</h3>
+                <p>Tropic areas urge urban workforce to channel inherenrt mindset into...</p>
+              </S.NewsLeft>
+              <S.NewsRight>
+                <img src="/assets/screen/News-1.png" alt="news" />
+              </S.NewsRight>
+            </S.SingleNewsEl>
+            <S.SingleNewsEl>
+              <S.NewsLeft>
+                <h3>FloraFeed</h3>
+                <p>Concerns grow as famed Rafflesia, supported by Jinhua, loses smell</p>
+              </S.NewsLeft>
+              <S.NewsRight>
+                <img src="/assets/screen/News-2.png" alt="news" />
+              </S.NewsRight>
+            </S.SingleNewsEl>
+          </S.NewsSection>
+          <S.WeatherSection>
+            <S.WeatherUpper>
+              <S.WeatherLeft>
+                <img src="/assets/screen/sunny.svg" alt="Sunny" />
+              </S.WeatherLeft>
+              <S.WeatherRight>
+                <h1>32ºC</h1>
+                <h2>sun showers</h2>
+                <h3>expect weather to affect circadian rhythm of resting plants</h3>
+                <S.WeatherLower>
+                  <img src="/assets/screen/graph.svg" alt="graph" />
+                  <p>caution! higher than average precipitation</p>
+                </S.WeatherLower>
+              </S.WeatherRight>
+            </S.WeatherUpper>
+          </S.WeatherSection>
+        </S.BottomInfo>
+      </S.Right>
+      {/* <S.Footer>Powered by Jinhua Group Ltd.</S.Footer> */}
     </S.Container>
+  );
+}
+
+function Currency() {
+  return (
+    <S.CurrencyContainer>
+      <S.Title>
+        <h1>{"Currency Conversion"}</h1>
+      </S.Title>
+
+      <S.Table>
+        <S.LeftColumn>
+          <S.TableTitle>Donation per Unit</S.TableTitle>
+          <S.Item up={true}>
+            Breath <span>per exhale</span>
+          </S.Item>
+          <S.Item up={false}>
+            Atenttion <span>per hr</span>
+          </S.Item>
+
+          <S.Item up={false}>
+            Blood plasma <span>per ml</span>
+          </S.Item>
+          <S.Item up={false}>
+            Hair follicle <span>per g</span>
+          </S.Item>
+          <S.Item up={true}>
+            Nail bed <span>per g</span>
+          </S.Item>
+          <S.Item up={false}>
+            Activated soil <span>per kg</span>
+          </S.Item>
+        </S.LeftColumn>
+        <S.RightColumn>
+          <S.TableTitle>
+            {" "}
+            <img src="/assets/screen/osc.svg" alt="osc" />
+          </S.TableTitle>
+          <S.Item up={true}>
+            <img src="/assets/screen/osc-up.svg" alt="osc" />
+            <S.Span>1.5</S.Span>
+            <b>
+              <TbTriangleFilled />
+            </b>
+          </S.Item>
+          <S.Item up={false}>
+            <img src="/assets/screen/osc-down.svg" alt="osc" />
+            <S.Span>90</S.Span>
+            <b>
+              <TbTriangleInvertedFilled />
+            </b>
+          </S.Item>
+          <S.Item up={false}>
+            <img src="/assets/screen/osc-down.svg" alt="osc" />
+            <S.Span>45</S.Span>
+            <b>
+              <TbTriangleInvertedFilled />
+            </b>
+          </S.Item>
+          <S.Item up={false}>
+            <img src="/assets/screen/osc-down.svg" alt="osc" />
+            <S.Span>18</S.Span>
+            <b>
+              <TbTriangleInvertedFilled />
+            </b>
+          </S.Item>
+          <S.Item up={true}>
+            <img src="/assets/screen/osc-up.svg" alt="osc" />
+            <S.Span>16</S.Span>
+            <b>
+              <TbTriangleFilled />
+            </b>
+          </S.Item>
+          <S.Item up={false}>
+            <img src="/assets/screen/osc-down.svg" alt="osc" />
+            <S.Span>3</S.Span>
+            <b>
+              <TbTriangleInvertedFilled />
+            </b>
+          </S.Item>
+        </S.RightColumn>
+      </S.Table>
+    </S.CurrencyContainer>
   );
 }
--- containers/screen/Waiting/styles.js
+++ containers/screen/Waiting/styles.js
@@ -7,6 +7,8 @@ import { Appear } from "@/styles/common";
 export const Container = styled.div`
   ${FlexCenterStyle};
   ${WholeContainer};
+  color: #fff0e8;
+  font-family: Poppins;
 `;
 
 export const LogoContainer = styled.div`
@@ -18,4 +20,341 @@ export const LogoContainer = styled.div`
   img {
     width: 15vw;
   }
+`;
+
+export const Left = styled.div`
+  width: 23vw;
+  height: calc(100% - 7vw);
+  margin-top: 7vw;
+  ${FlexCenterStyle};
+  flex-direction: column;
+`;
+
+export const CurrencyContainer = styled.div`
+  position: relative;
+  width: 23vw;
+  background: rgba(0, 0, 0, 0.5);
+  ${FlexCenterStyle};
+  flex-direction: column;
+`;
+
+export const Table = styled.div`
+  ${FlexCenterStyle};
+  margin: 1vw;
+  margin-bottom: 1.5vw;
+`;
+
+export const LeftColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 13vw;
+`;
+
+export const RightColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 5vw;
+`;
+
+export const TableTitle = styled.div`
+  font-family: Poppins;
+  font-weight: semi-bold;
+  margin: 0.5vw 0;
+  height: 3vw;
+  display: flex;
+  align-items: center;
+  border-bottom: 0.1vw solid #fff0e8;
+`;
+
+export const Item = styled.div`
+  font-family: Newsreader;
+  color: #ddd;
+  margin: 0.2vw 0;
+  height: 1.4vw;
+  font-size: 1.3vw;
+  display: flex;
+  align-items: center;
+  span {
+    font-size: 1.1vw;
+    opacity: 0.7;
+    margin-left: 0.3vw;
+    padding-left: 0.3vw;
+  }
+
+  b {
+    font-size: 1vw;
+    margin-left: 0.6vw;
+    margin-top: 0.3vw;
+  }
+
+  img {
+    width: 0.9vw;
+    margin-top: 0.1vw;
+    opacity: 0.9;
+    margin-right: 0.3vw;
+  }
+
+  ${({ up }) => (up ? "color: hsl(346, 87%, 83%);" : "color: hsl(160, 87%, 83%);")}
+`;
+
+export const Span = styled.div`
+  font-size: 1.3vw !important;
+  opacity: 1 !important;
+  margin: 0 !important;
+  width: 3vw !important;
+`;
+
+export const Title = styled.div`
+  ${FlexCenterStyle};
+  flex-direction: column;
+  height: 4vw;
+  margin-top: -2vw;
+  width: 16vw;
+
+  background: linear-gradient(92.1deg, #9a2901 13.61%, #e25b2d 68.62%, #f0b6ab 129.97%);
+  border: 0.1vw solid rgba(255, 240, 232, 0.35);
+  border-radius: 1vw;
+  font-family: Poiret One;
+
+  h1 {
+    font-size: 1.4vw;
+    margin: 0;
+  }
+  p {
+    font-size: 1.1vw;
+  }
+`;
+
+export const Commercial = styled.div`
+  width: 100%;
+  margin: 1vw 0;
+  height: 11vw;
+  background: rgba(0, 0, 0, 0.5);
+`;
+
+export const QRContainer = styled.div`
+  ${FlexCenterStyle};
+  width: 100%;
+  margin-top: 1vw;
+`;
+
+export const QRWrapper = styled.div`
+  width: 8vw;
+  height: 8vw;
+
+  img {
+    width: 8vw;
+    height: 8vw;
+  }
+`;
+
+export const Text = styled.div`
+  margin-left: 1.5vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  font-size: 1vw;
+  height: 8vw;
+  line-height: 1.1;
+`;
+
+export const Right = styled.div`
+  width: 70vw;
+  height: 100%;
+  ${FlexCenterStyle};
+  flex-direction: column;
+  h1 {
+    margin: 1vw 0;
+    font-family: Poiret One;
+    font-size: 2.5vw;
+    text-align: center;
+    color: #fff0e8;
+  }
+`;
+export const RankingSection = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  margin-top: 1vw;
+`;
+
+export const SingleEl = styled.div`
+  width: 18vw;
+  height: 10.125vw;
+  margin: 0.3vw 0.5vw;
+  background: rgba(0, 0, 0, 0.5);
+  position: relative;
+
+  ${({ border }) => `border: .4vw solid ${border ? "white" : "transparent"};`}
+`;
+
+export const Ranking = styled.div`
+  position: absolute;
+  top: -2.5vw;
+  left: -2.5vw;
+  width: 5vw;
+  height: 5vw;
+  ${FlexCenterStyle};
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    margin: auto;
+    width: ${({ big }) => (big ? "5vw" : "3vw")};
+    height: ${({ big }) => (big ? "5vw" : "3vw")};
+  }
+`;
+
+export const BottomInfo = styled.div`
+  ${FlexCenterStyle};
+  height: 25vh;
+  margin-top: 1vw;
+  width: 58vw;
+
+  //align
+  justify-content: space-between;
+`;
+
+export const NewsSection = styled.div`
+  ${FlexCenterStyle};
+  flex-direction: column;
+`;
+
+export const SingleNewsEl = styled.div`
+  width: 25vw;
+  height: 5vw;
+  border-radius: 0.8vw;
+  background: #fff0e8;
+  margin: 0.2vw 0;
+  ${FlexCenterStyle};
+`;
+
+export const NewsLeft = styled.div`
+  text-align: left;
+  margin-left: 1vw;
+  width: 22vw;
+  h3 {
+    font-family: Newsreader;
+    font-size: 0.8vw;
+    color: #b3b3b3;
+  }
+  p {
+    font-family: Poppins;
+    font-size: 0.9vw;
+    color: #333;
+  }
+`;
+export const NewsRight = styled.div`
+  width: 4.4vw;
+  height: 4.4vw;
+  margin-right: 0.3vw;
+  border-radius: 0.8vw;
+  margin-top: 0.3vw;
+  margin-bottom: 0.3vw;
+
+  img {
+    width: 4.4vw;
+    height: 4.4vw;
+  }
+`;
+
+export const WeatherSection = styled.div`
+  ${FlexCenterStyle};
+  flex-direction: column;
+  width: 35vw;
+  margin-left: 5vw;
+`;
+export const WeatherUpper = styled.div`
+  ${FlexCenterStyle};
+  height: 5vw;
+`;
+
+export const WeatherLeft = styled.div`
+  width: 8vw;
+  height: 8vw;
+  margin-right: 1vw;
+  ${FlexCenterStyle};
+  img {
+    width: 8vw;
+    height: 8vw;
+  }
+
+  animation: image-rotate-slightly 3s infinite;
+
+  @keyframes image-rotate-slightly {
+    0% {
+      transform: rotate(0deg);
+    }
+    20% {
+      transform: rotate(12deg);
+    }
+    40% {
+      transform: rotate(0deg);
+    }
+    60% {
+      transform: rotate(-12deg);
+    }
+    80% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+`;
+
+export const WeatherRight = styled.div`
+  font-family: Poppins;
+  display: flex;
+  flex-direction: column;
+  margin-left: 2vw;
+
+  text-align: left;
+  h1 {
+    color: #333333;
+    text-align: left;
+    margin: 0;
+    font-size: 2vw;
+  }
+
+  h2 {
+    color: #4e4d4d;
+    margin: 0;
+    font-size: 1vw;
+  }
+  h3 {
+    color: #4e4d4d;
+    opacity: 0.7;
+    font-size: 0.9vw;
+    line-height: 0.95;
+  }
+`;
+export const WeatherLower = styled.div`
+  ${FlexCenterStyle};
+  flex-direction: column;
+
+  img {
+    width: 100%;
+  }
+
+  p {
+    color: #ac4141;
+    text-align: center;
+    font-size: 0.7vw;
+    line-height: 0.95;
+    margin-top: 0.5vw;
+  }
+`;
+
+export const Footer = styled.div`
+  position: absolute;
+  bottom: 1.5vw;
+  width: 100%;
+  color: #fff0e8;
+  opacity: 0.7;
+  font-size: 1.3vw;
+  text-align: center;
 `;
