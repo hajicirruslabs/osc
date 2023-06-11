a6efa954d74e86356040351e7b5b5482b9530232
ericggul <57379903+ericggul@users.noreply.github.com>
2023-06-11 13:35:10 +0100

    Fix: Socket Debugging

--- utils/hooks/sockets/screen/useSocketLiveStream.js
+++ utils/hooks/sockets/screen/useSocketLiveStream.js
@@ -10,9 +10,7 @@ export default function useSocketInit({ handleNewHeart }) {
   const socketInitializer = async () => {
     await fetch("/api/socket");
 
-    socket.current = io({
-      transports: ["websocket"],
-    });
+    socket.current = io();
 
     socket.current.on("connect", () => {
       console.log("socket connected");
--- utils/hooks/sockets/useSocketMobile.js
+++ utils/hooks/sockets/useSocketMobile.js
@@ -4,24 +4,23 @@ import { useEffect, useRef } from "react";
 export default function useSocketInit() {
   const socket = useRef(null);
   useEffect(() => {
-    const socketInitializer = async () => {
-      await fetch("/api/socket");
-
-      socket.current = io({
-        transports: ["websocket"],
-      });
-
-      socket.current.on("connect", () => {
-        console.log("socket connected");
-      });
-
-      socket.current.on("connect_error", (err) => {
-        console.log(`connect_error due to ${err.message}`);
-      });
-      console.log(socket.current);
-    };
     socketInitializer();
   }, []);
+
+  async function socketInitializer() {
+    await fetch("/api/socket");
+
+    socket.current = io()
+
+    socket.current.on("connect", () => {
+      console.log("socket connected");
+    });
+
+    socket.current.on("connect_error", (err) => {
+      console.log(`connect_error due to ${err.message}`);
+    });
+    console.log(socket.current);
+  }
 
   return socket;
 }
