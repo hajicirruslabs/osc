153fd2c7d9984b74eb170f3c7d43e001dbe064ad
Haji Uduman <haji.uduman@cirruslabs.io>
2023-09-23 08:48:00 +0530

    added https support

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const { createServer } = require('http')
 const { parse } = require('url')
-const next = require('next')
+const next = require('next');
+const { createServer: httpsCreateServer } = require('https');
+const fs = require("fs");
  
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
-const port = 3000
+const port = 3001
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
@@ -37,4 +39,35 @@ app.prepare().then(() => {
     .listen(port, () => {
       console.log(`> Ready on http://${hostname}:${port}`)
     })
+
+    httpsCreateServer({
+        key: fs.readFileSync('./.ssl/server.key'),
+        cert: fs.readFileSync('./.ssl/server.cert')
+    }, async (req, res) => {
+        try {
+          // Be sure to pass `true` as the second argument to `url.parse`.
+          // This tells it to parse the query portion of the URL.
+          const parsedUrl = parse(req.url, true)
+          const { pathname, query } = parsedUrl
+     
+          if (pathname === '/a') {
+            await app.render(req, res, '/a', query)
+          } else if (pathname === '/b') {
+            await app.render(req, res, '/b', query)
+          } else {
+            await handle(req, res, parsedUrl)
+          }
+        } catch (err) {
+          console.error('Error occurred handling', req.url, err)
+          res.statusCode = 500
+          res.end('internal server error')
+        }
+      })
+        .once('error', (err) => {
+          console.error(err)
+          process.exit(1)
+        })
+        .listen(4443, () => {
+          console.log(`> Ready on https://${hostname}:4443`)
+        })
 })
